e9993a7b348f31c121bed6bed6eebb9f94928fbd
Olaf Hagendorf <olaf.hagendorf@hs-wismar.de>
2021-07-13 19:19:52 +0200

    adding HTU21 sensor example

---
+++ https_send_HTU21_batch/exportv2.bat
@@ -0,0 +1,37 @@
+echo off
+rem +++++++++ config ++++++++++
+rem +++++++++++++++++++++++++++
+
+set mbedos=mbed-os-6.12.0
+
+set platform=NUCLEO_F767ZI
+rem set platform=AUTO
+
+set ide=uvision6
+rem set ide=eclipse_gcc_arm
+rem set ide=sw4stm32
+rem set ide=gcc_arm
+
+set exp=debug
+rem set exp=develop
+rem set exp=release
+
+rem +++++++++++++++++++++++++++
+rem +++++++ config ende +++++++
+
+for %%I in (.) do (
+   set projectpath=%%~dpI
+   set prj=%%~nxI
+)
+
+cd ..
+
+echo:"Mbed OS:   "%mbedos%
+echo:"Plattform: "%platform%
+echo:"IDE:       "%ide%
+
+mbed config -G MBED_OS_DIR %projectpath%\..\%mbedos%
+mbed export -v -m %platform% -i %ide% --source .\%prj% --source ..\%mbedos%
+
+cd .\%prj%
+pause 
